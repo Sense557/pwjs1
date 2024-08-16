03a0e7022d2c179fb3dcbb0692d8412ab31c60c5
Sense <104638287+Sense557@users.noreply.github.com>
2024-08-16 18:46:27 +0000

    number discussion

---
+++ pwbasics1/numAndString.js
@@ -0,0 +1,19 @@
+const firstName = "Hello";
+const lastName = "SM";
+
+console.log(firstName + ' ' + lastName);
+console.log(`My firstname is ${firstName} and my lastname is ${lastName}`);
+
+
+const username = new String ('shaktidotcom');
+console.log(username[0]);
+console.log(username.length);
+
+const num = new Number (5);
+console.log(num.toFixed(2));
+
+const subscribers = 100000000;
+console.log(subscribers.toLocaleString());
+
+
+console.log(Math.floor(Math.random()*10)+1);
